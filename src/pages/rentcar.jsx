f63cbbec3d1ca474842a683c4b2ae73c0e9f4067
FacundoCastillo12 <facuelfacha15@gmail.com>
2023-03-11 18:39:52 -0300

    add pages

---
+++ src/pages/rentcar.jsx
@@ -0,0 +1,65 @@
+import Image from "next/image";
+import React from "react";
+import rentACarImg from "../../public/assents/images/projects/rent-a-car.png";
+import { RiRadioButtonFill } from "react-icons/ri";
+import Link from "next/link";
+const rentcar = () => {
+  return (
+    <div className="w-full">
+      <div className="w-screen h-[30vh] lg:h-[40vh] relative">
+        <div className="absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10">
+          <Image
+            className="absolute z-1 inset-0 w-full h-full object-cover object-top"
+            src={rentACarImg}
+            alt="/"
+          />
+          <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
+            <h2 className="py-2">Rent a Car</h2>
+            <h3>Node JS y Express JS</h3>
+          </div>
+        </div>
+      </div>
+      <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8">
+        <div className="col-span-4">
+          <p>Proyecto</p>
+          <h2>Resumen</h2>
+          <p>
+            Este proyecto fue construido usando Node JS y Express JS junto a
+            nunjucks que renderiza las plantillas. Los datos se guardan en una
+            base de datos de sqlite y mi objetivo con este proyecto era aprender
+            por primera vez back-end.
+          </p>
+          <button className="px-8 py-2 mt-4 mr-8">Demo</button>
+          <button className="px-8 py-2 mt-4 mr-8">Code</button>
+        </div>
+        <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4">
+          <div className="p-2">
+            <p className="text-center font-bold pb-2">Tecnologias</p>
+            <div className="grid grid-cols-3 md:grid-cols-1">
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Node JS
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Express JS
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Sqlite
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Nunjucks
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Pruebas con Jest
+              </p>
+            </div>
+          </div>
+        </div>
+        <Link href="/#projects">
+          <p className="underline cursor-pointer">Retroceder</p>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default rentcar;
